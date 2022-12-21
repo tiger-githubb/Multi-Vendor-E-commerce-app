68853e8a88419f2fade87cc2e87c8f4c5f953d1b
KARBOU Aristide <aristidetiger12@gmail.com>
2022-12-21 08:23:52 +0000

    first part of admin login and data insert with seeder

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
+// class Admin extends Authenticatable
 class Admin extends Model
 {
-    use HasFactory;
+    use HasFactory; 
+    protected $guard = 'admin';
 }
